9d791d48f5ddd163849188a4a269749776a71418
Michael Eden <Mikey.Eden@gmail.com>
2020-02-02 01:03:48 -0500

    Fixes to deployment script

--- deploy.py
+++ deploy.py
@@ -3,26 +3,34 @@ import shutil
 import subprocess
 
 
+FNULL = open(os.devnull, 'w')
+
+
+# TODO: error handling
 def deploy():
+    build_path = os.path.abspath('./build')
+
     print('1. Removing build folder')
-    shutil.rmtree('./build')
+    if os.path.isdir(build_path):
+        shutil.rmtree(build_path)
     
     print('2. Compiling project to build folder')
-    run_bash_cmd('tsc index.ts')
+    os.mkdir(build_path)
+    run_bash_cmd('tsc --outDir build index.ts')
 
     print('3. Creating zip')
-    run_bash_cmd('zip -r lambdabuild . ../node_modules', cwd='./build').wait()
+    run_bash_cmd('zip -r lambdabuild . ../node_modules', cwd=build_path, stdout=FNULL)
 
     print('4. Uploading to AWS')
-    run_bash_cmd('aws lambda update-function-code --function-name InquestCrud --zip-file fileb://lambdabuild.zip')
+    run_bash_cmd('aws lambda update-function-code --function-name InquestCrud --zip-file fileb://build/lambdabuild.zip')
 
     print('5. Cleaning up')
-    shutil.rmtree('./build')
-    os.remove('lambdabuild.zip')
+    shutil.rmtree(build_path)
 
 
-def run_bash_cmd(cmd, cwd='.'):
-    subprocess.Popen(cmd, cwd=cwd).wait()
+def run_bash_cmd(cmd, cwd=None, stdout=None):
+    args = cmd.split()
+    subprocess.Popen(args, stdout=stdout, cwd=cwd).wait()
 
 
 if __name__ == '__main__':
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 console.log('Loading function');
 
+import app from './app/app';
+
 const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
 
 const handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
