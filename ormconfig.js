79c2b56d7030cab254830c641afeb112de6a8bca
Michael Eden <michael.eden100@gmail.com>
2020-10-02 15:42:14 -0400

    Update ormconfig.js to connect to Heroku database instance

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,10 @@
 module.exports = {
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
+  // TODO: disable.
   synchronize: false,
-  logging: true, // TODO: disable in production.
+  // TODO: disable in production.
+  logging: true,
   entities: ['build/entity/**/*.js'],
   cli: { entitiesDir: 'src/entity' },
 };
