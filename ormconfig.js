06cc13e02713be60e4cdfc94996be256163f1960
Michael Eden <michael.eden100@gmail.com>
2020-10-03 13:30:26 -0400

    Disable database logging

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,8 @@
 module.exports = {
   type: 'mysql',
   url: process.env.DATABASE_URL,
-  // TODO: disable.
   synchronize: false,
-  // TODO: disable in production.
-  logging: true,
+  logging: false,
   entities: ['build/entity/**/*.js'],
   cli: { entitiesDir: 'src/entity' },
 };
