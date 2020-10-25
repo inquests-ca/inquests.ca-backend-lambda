0d4f61217662be1b48648cf300b2978436a1e4f0
Michael Eden <michael.eden100@gmail.com>
2020-10-25 17:16:56 -0400

    Update comment in sql.ts

--- src/utils/sql.ts
+++ src/utils/sql.ts
@@ -1,2 +1,2 @@
-// Replace all non-word characters with a space.
+// Escapes string for MySQL regex by replacing all non-word characters with a space.
 export const escapeRegex = (term: string): string => term.replace(/[^A-Za-z0-9]/g, ' ').trim();
